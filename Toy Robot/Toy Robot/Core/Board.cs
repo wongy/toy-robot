28a5830dc6c3b6bab8ffc2e5371967c6e7f02914
Wilson Wong <wilson.wong@findex.com.au>
2022-03-20 23:37:42 +1100

    added validate position function for board and placeholder functions for simulator

--- Toy Robot/Toy Robot/Core/Board.cs
+++ Toy Robot/Toy Robot/Core/Board.cs
@@ -2,18 +2,26 @@
 {
     class Board
     {
-        public int Length { get; set; }
-        public int Width { get; set; }
+        // constants for board dimensions boundaries
+        private const int MinX = 0;
+        private const int MinY = 0;
 
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+
+        // 
         public Board(int length = 6, int width = 6)
         {
-            Length = length;
-            Width = width;
+            MaxX = length - 1; // the maximum X coordinate is the length of the board minus 1
+            MaxY = width - 1; // the maximum Y coordinate is the width of the board minus 1
         }
 
-        public bool IsValidPlacement()
+        // does a check to see if the provided placement position is within the maximum length and width of the board
+        public bool IsValidPlacement(int XCoordinate, int YCoordinate)
         {
-            return true;
+            var ValidXPosition = XCoordinate >= MinX && XCoordinate <= MaxX;
+            var ValidYPosition = YCoordinate >= MinY && YCoordinate <= MaxY;
+            return ValidXPosition && ValidYPosition;
         }
     }
 }
--- Toy Robot/Toy Robot/Core/Simulator.cs
+++ Toy Robot/Toy Robot/Core/Simulator.cs
@@ -1,10 +1,52 @@
-﻿namespace Toy_Robot.Core
+﻿using Toy_Robot.Core.Interfaces;
+
+namespace Toy_Robot.Core
 {
     class Simulator
     {
-        public Simulator()
-        {
+        // Messages for invalid cases
+        public const string InvalidCommandMessage = "Command is invalid and is ignored.";
+        public const string NotPlacedMessage = "Command is invalid as robot is not placed yet.";
+        public const string OutOfBoundsMessage = "Move command is ignored as robot will be going out of bounds.";
 
+        private enum Commands
+        {
+            Place,
+            Left,
+            Right,
+            Move,
+            Report
+        }
+
+        public Board _board;
+        public IRobot _robot;
+
+        private bool isRobotPlaced = false;
+
+        public Simulator(Board board)
+        {
+            _board = board;
+        }
+
+        // Used to read user input and decide which command to run
+        public string ProcessCommand(string command)
+        {
+            return "";
+        }
+
+        public string Place(string command)
+        {
+            return "";
+        }
+
+        public string Move()
+        {
+            return "";
+        }
+
+        private bool IsValidPlaceCommand()
+        {
+            return true;
         }
     }
 }
